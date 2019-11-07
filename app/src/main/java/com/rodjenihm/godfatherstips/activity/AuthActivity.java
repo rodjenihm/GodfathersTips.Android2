6f28853d56c8b8ba032e93cb092e1fb596b73072
Данијел Митровић <rodjenihm@outlook.com>
2019-11-08 00:14:20 +0100

    Update 1: Add AuthActivity; add AppTheme.NoActionBar style

---
+++ app/src/main/java/com/rodjenihm/godfatherstips/activity/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.rodjenihm.godfatherstips.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rodjenihm.godfatherstips.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
